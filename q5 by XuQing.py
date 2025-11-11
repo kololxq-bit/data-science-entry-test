1b8ee8471306c43724bee22a6db48989c52913a5
kololxq-bit <kololxq21@gmail.com>
2025-11-11 11:14:29 +0800

    Create q5 by XuQing.py

---
+++ q5 by XuQing.py
@@ -0,0 +1,21 @@
+def check_divisibility(num, divisor):
+    """
+    Task 1
+    - Create a function to check if the number (num) is divisible by another number (divisor).
+    - Both num and divisor must be numeric.
+    - Return True if num is divisible by divisor, False otherwise.
+    """
+    try:
+        # Try to perform the modulo operation
+        return num % divisor == 0
+    except:
+        #return error if argument is not numeric
+        return str('not numeric')
+
+# Task 2
+# Invoke the function "check_divisibility" using the following scenarios:
+# - 10, 2
+# - 7, 3
+
+print(check_divisibility(10, 2))  # True
+print(check_divisibility(7, 3))   # False
